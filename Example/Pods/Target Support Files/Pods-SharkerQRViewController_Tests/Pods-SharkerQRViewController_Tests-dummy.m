47f9fccd9fc40a4485f7d1cf3b6cdead32f0359e
liushuhao_i <liushuhao_i>
2019-01-27 23:27:42 +0800

    重试

--- Example/Pods/Target Support Files/Pods-SharkerQRViewController_Tests/Pods-SharkerQRViewController_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SharkerQRViewController_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SharkerQRViewController_Tests
-@end
--- Example/Pods/Target Support Files/SharkerQRViewController/SharkerQRViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SharkerQRViewController : NSObject
-@end
-@implementation PodsDummy_SharkerQRViewController
-@end
